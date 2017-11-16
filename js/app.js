aa39d3b73a7bfe9920433e6559ed5e476cef444c
Anna Ploch <pplochanna@gmail.com>
2017-11-16 14:14:28 +0100

    update

--- js/app.js
+++ js/app.js
@@ -3,25 +3,32 @@
 });*/
 
 $(function () {
-    console.log("domOK");
-    
-//header
+    //console.log("domOK");
+
+    //header
     var $nav = $('.nav');
-
     $(window).scroll(function () {
-
         var $scroll = $(window).scrollTop();
-
         if (($scroll) >= 20) {
             $nav.addClass('nav--transition');
-
         } else {
             $nav.removeClass('nav--transition');
         }
     });
-      
-//slider
-    var $slider = $('.slider__content'); 
+
+    //smooth scroll  
+
+    var $smooth = $('.footer__smooth-up');
+    $smooth.on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+
+    //slider
+    var $slider = $('.slider__content');
     $slider.slick({
         ltr: true,
         autoplay: true,
@@ -30,20 +37,13 @@ $(function () {
         slidesToShow: 3,
         infinite: true
     });
-      
-//smooth scroll  
-    var $smooth = $('.footer__smooth-up');
-    $smooth.on('click', function(e){
-    e.preventDefault();
-		$('html, body').animate({scrollTop: 0}, 1000);
-	});
-    
-//cake background
-   /* var $box = ('.gallery__wrapper-item');
-    var $text = ('.gallery__item-text');
-    $box.mouseenter( function(){
-        console.log("tak");
-        $text.addClass('gallery__item-text--activ');    
-    })*/
-    
+
+    //cake background
+    /* var $box = ('.gallery__wrapper-item');
+     var $text = ('.gallery__item-text');
+     $box.mouseenter( function(){
+         console.log("tak");
+         $text.addClass('gallery__item-text--activ');    
+     })*/
+
 });
