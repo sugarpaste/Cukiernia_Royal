77cfc5a86ad3e351b8a25294764ed0879108ab35
Anna Ploch <pplochanna@gmail.com>
2018-01-23 01:59:25 +0100

    responsive part I

--- js/slider.js
+++ js/slider.js
@@ -9,7 +9,28 @@
            arrows: false,
            autoplaySpeed: 2500,
            slidesToShow: 3,
-           infinite: true
+           infinite: true,
+
+           responsive: [
+               {
+                   breakpoint: 1000,
+                   settings: {
+                       slidesToShow: 3,
+                       //slidesToScroll: 3,
+                   }
+    },
+               {
+                   breakpoint: 767,
+                   settings: {
+                       slidesToShow: 2
+                   }
+    },
+               {
+                   breakpoint: 480,
+                   settings: {
+                       slidesToShow: 1
+                   }
+               }]
        });
 
    });
