10783d6b08b1b90da7cadc45b8cd5784f90148f9
Anna Ploch <pplochanna@gmail.com>
2018-12-06 13:59:06 +0100

    christmas offer

--- js/slider_christmas.js
+++ js/slider_christmas.js
@@ -7,7 +7,7 @@
            ltr: true,
            autoplay: true,
            arrows: false,
-           autoplaySpeed: 3000,
+           autoplaySpeed: 4000,
            slidesToShow: 1,
            infinite: true,
 
