7c4b2e882ac15151cf6f296cceabf5118a04a806
Anna Ploch <pplochanna@gmail.com>
2019-02-26 22:23:44 +0100

    new slider

--- js/app.js
+++ js/app.js
@@ -10,39 +10,48 @@ $(function () {
     $(window).scroll(function () {
         var $scroll = $(window).scrollTop();
         var $width = $(window).width();
- 
+        var $header = $('.header');
+
         if (($scroll) >= 20 && ($width) > 767) {
             $nav.addClass('nav--transition');
         } else {
             $nav.removeClass('nav--transition');
         }
+
+        if (($scroll) >= 20) {
+            $header.css({
+                "background-color": "white",
+                "transition": "0.3s"})
+        } else {
+            $header.css("background-color", "rgba(255,255,255, 0.6)");
+        }
     });
 
     //cake background in main gallery
-   /* $('.gallery__wrapper-item').mouseenter(function () {
-        //console.log("tak");
-        $(this).find('.gallery__item-text').css({
-            "visibility": "visible"
-        });
-    });
-    $('.gallery__wrapper-item').mouseleave(function () {
-        //console.log("tak");
-        $(this).find('.gallery__item-text').css({
-            "visibility": "hidden"
-        });
-    });*/
-    
+    /* $('.gallery__wrapper-item').mouseenter(function () {
+         //console.log("tak");
+         $(this).find('.gallery__item-text').css({
+             "visibility": "visible"
+         });
+     });
+     $('.gallery__wrapper-item').mouseleave(function () {
+         //console.log("tak");
+         $(this).find('.gallery__item-text').css({
+             "visibility": "hidden"
+         });
+     });*/
+
     //cake background in main gallery
-    
+
     var $galleryItem = $('.gallery__wrapper-item');
-    
+
     $galleryItem.mouseenter(function () {
         $(this).addClass('gallery__wrapper-item--activ')
-        });
+    });
     $galleryItem.mouseleave(function () {
         $(this).removeClass('gallery__wrapper-item--activ')
     });
-    
+
 
     //smooth scroll  
     var $smooth = $('.footer__smooth-up');
@@ -57,9 +66,9 @@ $(function () {
     var $hamburger = $('.nav__mobile-link');
     var $nav = $('.nav');
     var $header = $('.header');
-    $hamburger.on('click', function (){
-      $nav.toggleClass('nav--open');  
-      $header.toggleClass('header--open');  
+    $hamburger.on('click', function () {
+        $nav.toggleClass('nav--open');
+        $header.toggleClass('header--open');
     })
 
 
